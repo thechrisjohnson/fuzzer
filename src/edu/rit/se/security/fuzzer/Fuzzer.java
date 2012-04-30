a099b3893378fd40964d82f0e8b76c321bc7034e
Christopher Johnson <cdj2981@gmail.com>
2012-04-30 19:51:21 -0400

    Initial creation of project and first class.

---
+++ src/edu/rit/se/security/fuzzer/Fuzzer.java
@@ -0,0 +1,18 @@
+package edu.rit.se.security.fuzzer;
+
+/**
+ * Main class for running the fuzzer.
+ * 
+ * @author Christopher Johnson <cdj2981@gmail.com>
+ */
+public class Fuzzer {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World!");
+	}
+
+}

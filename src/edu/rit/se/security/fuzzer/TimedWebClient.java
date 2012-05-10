bcb0ccb9c5adc9b4c0115e61b7c1ecba6683e471
Christopher Johnson <cdj2981@gmail.com>
2012-05-10 01:29:27 -0400

    Implemented feature to allow for a time span between requests.

--- src/edu/rit/se/security/fuzzer/TimedWebClient.java
+++ src/edu/rit/se/security/fuzzer/TimedWebClient.java
@@ -7,6 +7,8 @@ import com.gargoylesoftware.htmlunit.FailingHttpStatusCodeException;
 import com.gargoylesoftware.htmlunit.WebClient;
 import com.gargoylesoftware.htmlunit.html.HtmlPage;
 
+import edu.rit.se.security.fuzzer.properties.FuzzerProperties;
+
 /**
  * A wrapper class for the WebClient that allows for timing between requests
  * 
@@ -15,14 +17,55 @@ import com.gargoylesoftware.htmlunit.html.HtmlPage;
 public class TimedWebClient extends WebClient {
 	
 	private static final long serialVersionUID = -4710712450215994977L;
+	private boolean requestAllowed;
 
 	public TimedWebClient() {
 		super();
+		requestAllowed = true;
 	}
 	
 	public HtmlPage getPage(String url) throws FailingHttpStatusCodeException, MalformedURLException, IOException {
 		// TODO Timing things
+		while (!requestAllowed());
+		startTimer();
 		return super.getPage(url);
+		
+	}
+	
+	public synchronized boolean requestAllowed() {
+		return requestAllowed;
+	}
+	
+	public synchronized void setRequestAllowed(boolean requestAllowed) {
+		this.requestAllowed = requestAllowed;
+	}
+	
+	private void startTimer() {
+		setRequestAllowed(false);
+		Timer timer = new Timer(this);
+		
+		Thread thread = new Thread(timer);
+		
+		thread.start();
 	}
 
+}
+
+class Timer implements Runnable {
+	
+	TimedWebClient client;
+	
+	public Timer(TimedWebClient client) {
+		this.client = client;
+	}
+	
+	@Override
+	public void run() {
+		try {
+			Thread.sleep(FuzzerProperties.getTimeGap());
+		} catch (InterruptedException e) {
+			System.err.println("TimedWebClient : " + e.getMessage());
+		}
+		client.setRequestAllowed(true);
+	}
 }
